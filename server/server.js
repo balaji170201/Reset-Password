d0be0b00b9fb1c5d87977a9de56cfdd9c18cb7cd
Balaji <117513456+balaji170201@users.noreply.github.com>
2023-12-26 13:01:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://reset-password-auth.netlify.app/'],
     methods:['GET','POST'],
     credentials:true
 }));
